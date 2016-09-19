779490dae79d364ead381295be9dfb01ef2f37ab
Bass Jobsen <bass@w3masters.nl>
2016-09-19 10:03:33 +0200

    prefixed with your theme slug

--- assets/functions/editor-styles.php
+++ assets/functions/editor-styles.php
@@ -1,6 +1,6 @@
 <?php
 // Adds your styles to the WordPress editor
-add_action( 'init', 'add_editor_styles' );
+add_action( 'init', 'jbst4_add_editor_styles' );
 function add_editor_styles() {
-    add_editor_style( get_template_directory_uri() . '/assets/css/style.min.css' );
+    jbst4_add_editor_style( get_template_directory_uri() . '/assets/css/style.min.css' );
 }
--- assets/functions/page-title.php
+++ assets/functions/page-title.php
@@ -3,10 +3,10 @@
 // http://wordpress.stackexchange.com/questions/228183/custom-attribute-for-the-title-tag-with-wp-title
 if (has_action('wp_head','_wp_render_title_tag') == 1) {
     remove_action('wp_head','_wp_render_title_tag',1);
-    add_action('wp_head','custom_wp_render_title_tag_filtered',1);
+    add_action('wp_head','jbst4_custom_wp_render_title_tag_filtered',1);
 }
 
-function custom_wp_render_title_tag_filtered() {
+function jbst4_custom_wp_render_title_tag_filtered() {
     if (function_exists('_wp_render_title_tag')) {
         ob_start(); 
         _wp_render_title_tag(); 
@@ -16,9 +16,9 @@ function custom_wp_render_title_tag_filtered() {
     echo apply_filters('wp_render_title_tag_filter',$titletag);
 }
 
-add_filter('wp_render_title_tag_filter','custom_wp_render_title_tag');
+add_filter('wp_render_title_tag_filter','jbst4_custom_wp_render_title_tag');
 
-function custom_wp_render_title_tag($titletag) {
+function jbst4_custom_wp_render_title_tag($titletag) {
     $titletag = str_replace('<title>','<title itemprop="name">',$titletag);
     return $titletag;
 }
