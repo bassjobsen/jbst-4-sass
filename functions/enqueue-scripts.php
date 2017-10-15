576edaa208025149c3073a219cdaabe1e9010018
Bass Jobsen <bass@w3masters.nl>
2017-10-15 21:06:43 +0200

    add some untracked and move files

---
+++ functions/enqueue-scripts.php
@@ -0,0 +1,16 @@
+<?php
+function jbst4_site_scripts() {
+  global $wp_styles; // Call global $wp_styles variable to add conditional wrapper around ie stylesheet the WordPress way
+        
+    // Adding scripts file in the footer
+    wp_enqueue_script( 'site-js', get_template_directory_uri() . '/assets/scripts/scripts.js', array( 'jquery' ), filemtime(get_template_directory() . '/assets/scripts/js'), true );
+   
+    // Register main stylesheet
+    wp_enqueue_style( 'site-css', get_template_directory_uri() . '/assets/styles/style.css', array(), filemtime(get_template_directory() . '/assets/styles/scss'), 'all' );
+
+    // Comment reply script for threaded comments
+    if ( is_singular() AND comments_open() AND (get_option('thread_comments') == 1)) {
+      wp_enqueue_script( 'comment-reply' );
+    }
+}
+add_action('wp_enqueue_scripts', 'jbst4_site_scripts', 999);
